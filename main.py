9ab192d4eeefa5f7599fdb02019cb0d4383f3911
Pragadeesh Raj <pragadeesh3000@gmail.com>
2020-11-04 09:36:58 -0800

    embed

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-import projects #projects definitions are placed in different file
+import projects  # projects definitions are placed in different file
 
 # https://flask.palletsprojects.com/en/1.1.x/api/
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home_route():
     return render_template("home.html", projects=projects.setup())
 
+
 @app.route('/basic/')
-def hello_route():
+def basic_route():
     return render_template("basic.html", projects=projects.setup())
 
+
 @app.route('/scientific/')
-def flask_route():
+def scientific_route():
     return render_template("scientific.html", projects=projects.setup())
+
 
 if __name__ == "__main__":
     app.run(port='3000', host='0.0.0.0')
--- projects.py
+++ projects.py
@@ -1,46 +1,57 @@
 def setup():
-  name = "P2-Einsteins"
-  github = "https://github.com/PragadeeshRaj/Raj-Academy"
-  source = {"name": name, "github": github}
+    name = "P2-Einsteins"
+    github = "https://github.com/PragadeeshRaj/Raj-Academy"
+    embedbasic = "https://basiccalc.PragadeeshRaj.repl.run"
+    embedscientific = "https://Calc.aymankazi9.repl.run"
+    source = {"name": name, "github": github, "embedbasic": embedbasic, "embedscientific": embedscientific}
 
-  project1 =  "Basic Calculator"
-  projlinks1 = [
-    Link("Calculator", "https://repl.it/@AZPragTeam/ThisIsItBois")
-  ]
-  project2 =  "Scientific Calculator"
-  projlinks2 = [
-    Link("Calculator", "https://github.com/PragadeeshRaj/Raj-Academy")
-  ]
+    project1 = "Basic Calculator"
+    projlinks1 = [
+        Link("Calculator", "https://repl.it/@AZPragTeam/ThisIsItBois")
+    ]
+    project2 = "Scientific Calculator"
+    projlinks2 = [
+        Link("Calculator", "https://github.com/PragadeeshRaj/Raj-Academy")
+    ]
 
-  proj1 = Project(project1, projlinks1)
-  proj2 = Project(project2, projlinks2)
+    proj1 = Project(project1, projlinks1)
+    proj2 = Project(project2, projlinks2)
 
-  projects = Projects(source, [proj1, proj2])
-  return projects
+    projects = Projects(source, [proj1, proj2])
+    return projects
+
 
 class Link():
-  def __init__(self, btn, href):
-    self.btn = btn
-    self.href = href
-  def get_btn(self):
-    return self.btn
-  def get_href(self):
-    return self.href
+    def __init__(self, btn, href):
+        self.btn = btn
+        self.href = href
+
+    def get_btn(self):
+        return self.btn
+
+    def get_href(self):
+        return self.href
+
 
 class Project():
-  def __init__(self, name, links):
-    self.name = name
-    self.links = links
-  def get_name(self):
-    return self.name
-  def get_links(self):
-    return self.links
+    def __init__(self, name, links):
+        self.name = name
+        self.links = links
+
+    def get_name(self):
+        return self.name
+
+    def get_links(self):
+        return self.links
+
 
 class Projects():
-  def __init__(self, source, projects):
-    self.source = source
-    self.projects = projects
-  def get_source(self):
-    return self.source
-  def get_projects(self):
-    return self.projects
+    def __init__(self, source, projects):
+        self.source = source
+        self.projects = projects
+
+    def get_source(self):
+        return self.source
+
+    def get_projects(self):
+        return self.projects
